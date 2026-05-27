b392b9aff5c762a82cda821bca96dcb965de0ed0
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-05-27 13:46:42 -0300

    Create WebConfig.java

---
+++ src/main/java/com/space/sort/fiap/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.space.sort.fiap.config;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:19006", "http://192.168.100.113:19006")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
