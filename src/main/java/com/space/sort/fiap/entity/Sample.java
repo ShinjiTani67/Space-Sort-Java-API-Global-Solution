d0e0bb12fcdfd86657a3a2bbc19d74cc8e341a15
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-05-27 10:42:32 -0300

    update sample entity

--- src/main/java/com/space/sort/fiap/entity/Sample.java
+++ src/main/java/com/space/sort/fiap/entity/Sample.java
@@ -1,7 +1,16 @@
 package com.space.sort.fiap.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.ToString;
+
 import java.util.UUID;
 
+@Entity
+@Data
+@ToString
+@Table(name="tb_user")
 public class Sample {
     private String description;
     private UUID uuid;
