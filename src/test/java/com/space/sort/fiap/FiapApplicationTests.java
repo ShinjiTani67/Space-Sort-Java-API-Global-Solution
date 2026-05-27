ebc5cb7862b34b0e17901b8cbfbc19b483b3259a
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-05-27 10:29:24 -0300

    Initial commit

---
+++ src/test/java/com/space/sort/fiap/FiapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.sort.fiap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
