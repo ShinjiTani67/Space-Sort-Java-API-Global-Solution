c41b7ff80bfd46b8e513f527404ff2ef3ce7f8d4
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-05-27 12:42:56 -0300

    Fix import statement in UserRepository

--- src/main/java/com/space/sort/fiap/repository/UserRepository.java
+++ src/main/java/com/space/sort/fiap/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.space.sort.fiap.entity;
 
 import java.util.Optional;
 import java.util.UUID;
+import com.space.sort.fiap.entity;
 
 public interface UserRepository extends JpaRepository<User,UUID>{
 
