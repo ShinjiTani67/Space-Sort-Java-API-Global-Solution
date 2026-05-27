0d2c071ab57b505dc8444f04f20a05061531902d
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-05-27 10:37:27 -0300

    update user

--- src/main/java/com/space/sort/fiap/entity/User.java
+++ src/main/java/com/space/sort/fiap/entity/User.java
@@ -1,5 +1,20 @@
 package com.space.sort.fiap.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import java.util.UUID;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@ToString
+@Table(name="tb_user")
 public class User {
-    private final nome;
+
+    private String nome;
+
+    private UUID uuid;
 }
