373240420c271723dff5424f6609438b65acab45
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-05-27 12:37:04 -0300

    Create SampleDTO class with fields and annotations

---
+++ src/main/java/com/space/sort/fiap/dto/SampleDTO.java
@@ -0,0 +1,11 @@
+package com.space.sort.fiap.dto;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public Class SampleDTO{
+  private String name;
+  private UUID uuid;
+  private String description;
+  private LocalDate data;
+}
