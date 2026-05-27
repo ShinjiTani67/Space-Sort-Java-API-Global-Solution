7ee7ef83df085113361daa7542f7cd62ef27adbb
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-05-27 13:34:36 -0300

    Add SQL schema for sample and tb_account tables

---
+++ src/main/resources/schemas.sql
@@ -0,0 +1,15 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE sample (
+    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    description VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    owner VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tb_account (
+    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
