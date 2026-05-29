9469fb4c33bcf7d50b614fd71f8cb3b889e7bfe6
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-05-29 09:13:29 -0300

    correção de controles e paginas html

---
+++ src/main/java/com/space/sort/fiap/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.space.sort.fiap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+}
--- src/main/java/com/space/sort/fiap/controller/SampleController.java
+++ src/main/java/com/space/sort/fiap/controller/SampleController.java
@@ -1,18 +1,16 @@
 package com.space.sort.fiap.controller;
 
 import com.space.sort.fiap.dto.SampleDTO;
-import com.space.sort.fiap.dto.UserDTO;
 import com.space.sort.fiap.service.SampleService;
 import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/sample")
+@RequestMapping("/samples")
 @Log
 public class SampleController {
 
@@ -22,47 +20,65 @@ public class SampleController {
         this.service = service;
     }
 
-    @GetMapping("/list")
-    public List<SampleDTO> listSample() {
+    @ResponseBody
+    @GetMapping("/all")
+    public ResponseEntity<?> listSample() {
 
         var sampleList = service.getSample();
 
         sampleList.forEach(e ->
-                log.info("ID funcionário: " + e.getUuid())
+                log.info("ID sample: " + e.getUuid())
         );
 
-        return sampleList; 
+        return ResponseEntity.ok(sampleList);
     }
 
+    @ResponseBody
+    @GetMapping("/{uuid}")
+    public ResponseEntity<SampleDTO> getSampleById(
+            @PathVariable UUID uuid) {
+
+        return ResponseEntity.ok(
+                service.findById(uuid)
+        );
+    }
+
+    @ResponseBody
+    @PostMapping("/save")
+    public ResponseEntity<SampleDTO> saveSample(
+            @RequestBody SampleDTO sampleDTO) {
+
+        log.info("Salvando sample: " + sampleDTO);
+
+        return ResponseEntity.ok(
+                service.save(sampleDTO)
+        );
+    }
+
+    @ResponseBody
     @PutMapping("/{uuid}")
     public ResponseEntity<SampleDTO> update(
             @PathVariable UUID uuid,
             @RequestBody SampleDTO dto) {
 
-        return ResponseEntity.ok(service.update(uuid, dto));
+        return ResponseEntity.ok(
+                service.update(uuid, dto)
+        );
     }
 
-    @GetMapping("/{uuid}")
-    public UserDTO getUserById(@PathVariable UUID uuid) {
-        return service.findById(uuid);
-    }
-
-    @PostMapping("/save")
-    public SampleDTO saveSample(@RequestBody SampleDTO sampleDTO) {
-
-        log.info("Salvando funcionário: " + sampleDTO);
-
-        return "/return";
-    }
-
+    @ResponseBody
     @DeleteMapping("/delete/{uuid}")
-    public String deleteSample(@PathVariable UUID uuid) {
+    public ResponseEntity<String> deleteSample(
+            @PathVariable UUID uuid) {
 
         service.deleteById(uuid);
 
-        return "Funcionário deletado com sucesso";
+        return ResponseEntity.ok(
+                "Sample deletado com sucesso"
+        );
     }
 
+    @ResponseBody
     @GetMapping("/test")
     public String test() {
         return "Conectado com sucesso";
