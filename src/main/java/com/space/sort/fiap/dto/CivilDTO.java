db043e444c33813b5f180ebcb7dd5e3e79a0346a
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-06-03 14:18:08 -0300

    Add CivilDTO class for civil data representation

---
+++ src/main/java/com/space/sort/fiap/dto/CivilDTO.java
@@ -0,0 +1,16 @@
+package com.space.sort.fiap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CivilDTO{
+    private String name;
+    private UUID uuid;
+    private String email;
+}
