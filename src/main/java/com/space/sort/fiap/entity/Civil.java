bf57021e9c930413fa44f2928cf01e5f14981fe1
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-06-03 14:15:04 -0300

    Create Civil entity class for user management

---
+++ src/main/java/com/space/sort/fiap/entity/Civil.java
@@ -0,0 +1,27 @@
+package com.space.sort.fiap.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import java.util.UUID;
+import jakarta.persistence.Id;
+
+
+@Entity
+@Data
+@ToString
+@Table(name="tb_account")
+public class User {
+
+    @Column(nullable = false)
+    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+    @Column(nullable = false)
+    private String email;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.CIVIL;
+}
