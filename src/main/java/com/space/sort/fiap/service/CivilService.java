98058e57576ae6600608214acf83d6611387df21
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-06-03 14:37:47 -0300

    Implement CivilService for managing Civil entities

---
+++ src/main/java/com/space/sort/fiap/service/CivilService.java
@@ -0,0 +1,98 @@
+package com.space.sort.fiap.service;
+
+import com.space.sort.fiap.dto.CivilDTO;
+import com.space.sort.fiap.entity.Civil;
+import com.space.sort.fiap.repository.CivilRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CivilService {
+
+    private final CivilRepository repository;
+
+    private CivilDTO convertToDTO(Civil civil) {
+
+        CivilDTO dto = new CivilDTO();
+
+        dto.setUuid(civil.getUuid());
+        dto.setName(civil.getName());
+
+        return dto;
+    }
+
+    private Civil convertToEntity(CivilDTO dto) {
+
+        Civil civil = new Civil();
+
+        if (dto.getUuid() != null) {
+            civil.setUuid(dto.getUuid());
+        }
+
+        civil.setName(dto.getName());
+        civil.setEmail(dto.getEmail());
+        civil.setUuid(dto.getUuid());
+
+        return civil;
+    }
+
+    public CivilDTO save(CivilDTO civilDTO) {
+
+        Civil civil = convertToEntity(civilDTO);
+
+        civil = repository.save(civil);
+
+        return convertToDTO(civil);
+    }
+
+    public List<CivilDTO> getCivil() {
+
+        return repository.findAll()
+                .stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    // BUSCAR POR ID
+    public CivilDTO findById(UUID uuid) {
+
+        Civil civil = repository.findById(uuid)
+                .orElseThrow(() ->
+                        new RuntimeException(
+                                "Funcionário com id " + uuid + " não encontrado"
+                        )
+                );
+
+        return convertToDTO(civil);
+    }
+
+    public void deleteById(UUID uuid) {
+
+        if (!repository.existsById(uuid)) {
+            throw new RuntimeException(
+                    "Funcionário com id " + uuid + " não encontrado"
+            );
+        }
+
+        repository.deleteById(uuid);
+    }
+
+    public CivilDTO update(UUID uuid, CivilDTO dto) {
+
+        Civil civil = repository.findByUuid(uuid)
+                .orElseThrow(() ->
+                        new RuntimeException("Funcionário não encontrado"));
+
+        civil.setName(dto.getName());
+        civil.setEmail(dto.getEmail());
+
+        civil = repository.save(civil);
+
+        return convertToDTO(civil);
+    }
+}
