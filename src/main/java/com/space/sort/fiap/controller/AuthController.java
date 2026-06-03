28e0e3ed781bd1c720fdba415ac0c466480f18a5
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-06-03 10:45:45 -0300

    create authcontroller

---
+++ src/main/java/com/space/sort/fiap/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.space.sort.fiap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "user-home";
+    }
+}
