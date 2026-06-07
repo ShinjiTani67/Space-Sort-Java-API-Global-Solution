eafb88841c25b77bea03499a242f74b1716bd475
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-06-07 11:54:38 -0300

    Refactor CustomUserDetailsService to use UserRepository

--- src/main/java/com/space/sort/fiap/service/CustomUserDetailsService.java
+++ src/main/java/com/space/sort/fiap/service/CustomUserDetailsService.java
@@ -9,56 +9,26 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
-public class CustomUserDetailsService
-        implements org.springframework.security.core.userdetails.UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
-    private final CivilRepository civilRepository;
-    private final AstronautRepository astronautRepository;
+    private final UserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
 
-        System.out.println("Tentando login com: " + email);
+        User user = repository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Usuário não encontrado"));
 
-        Optional<Civil> civil = civilRepository.findByEmail(email);
-
-        if (civil.isPresent()) {
-
-            System.out.println("Civil encontrado");
-            System.out.println("Senha banco: " + civil.get().getPassword());
-            System.out.println("Role banco: " + civil.get().getRole());
-
-            return new UserDetailsImpl(
-                    civil.get().getEmail(),
-                    civil.get().getPassword(),
-                    civil.get().getRole()
-            );
-        }
-
-        Optional<Astronaut> astronaut =
-                astronautRepository.findByEmail(email);
-
-        if (astronaut.isPresent()) {
-
-            System.out.println("Astronauta encontrado");
-            System.out.println("Senha banco: " + astronaut.get().getPassword());
-            System.out.println("Role banco: " + astronaut.get().getRole());
-
-            return new UserDetailsImpl(
-                    astronaut.get().getEmail(),
-                    astronaut.get().getPassword(),
-                    astronaut.get().getRole()
-            );
-        }
-
-        System.out.println("Usuário não encontrado");
-
-        throw new UsernameNotFoundException("Usuário não encontrado");
+        return new UserDetailsImpl(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole()
+        );
     }
 }
